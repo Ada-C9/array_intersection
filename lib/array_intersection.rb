86a736049064044040890e948c62eac829b0509c
Brandy Austin <brandy.austin.seattle@gmail.com>
2018-03-26 15:01:01 -0700

    All tests passing

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,24 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+
+    intersection = []
+
+    return intersection if array1.nil? || array2.nil?
+
+    index = 0
+
+    until index == array1.length
+
+      array2.each do |num|
+        if num == array1[index]
+          intersection << num
+        end
+      end
+
+      index += 1
+
+    end
+
+    return intersection
+
 end
