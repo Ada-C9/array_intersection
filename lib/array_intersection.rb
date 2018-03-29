fd758c2db5532e5a4d69272f8d65cbf75928225a
Jackie <jonofre16@hotmail.com>
2018-03-28 18:02:40 -0700

    Jackie - array-intersection - Ampers

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,19 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  if array1.class != Array || array2.class != Array
+    return []
+  end
+
+  array = []
+  
+  array1.each do |num|
+    array2.each do |num2|
+      if num == num2
+        array << num2
+      end
+    end
+  end
+
+  return array
+
 end
