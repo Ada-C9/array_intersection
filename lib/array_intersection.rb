3d6cdb8e92583aa0340190a749ddcf9b88240c42
Phoebe <phoebe.manning8@gmail.com>
2018-04-01 14:18:29 -0700

    picks smaller array for iteration

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -11,17 +11,34 @@ def intersection(array1, array2)
   len2 = array2.length
 
   i = 0
-  while i < len1
-    j = 0
-    while j < len2
-      if array1[i] == array2[j]
-         common_elements << array2[j]
-         array2.delete_at(j)
+  if len1 <= len2
+    while i < len1
+      j = 0
+      while j < len2
+        if array1[i] == array2[j]
+           common_elements << array2[j]
+           array2.delete_at(j)
+        end
+        j += 1
       end
-      j += 1
+      i += 1
     end
-    i += 1
+  else
+    while i < len2
+      j = 0
+      while j < len1
+        if array2[i] == array1[j]
+           common_elements << array1[j]
+           array1.delete_at(j)
+        end
+        j += 1
+      end
+      i += 1
+    end
+
   end
+
+
 
   return common_elements
 end
