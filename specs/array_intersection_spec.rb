f1915db816e4463c1abcdf351c5038690185b2bc
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-02 22:05:43 -0700

    Unsatisfying solution, but it's passing all tests.

--- specs/array_intersection_spec.rb
+++ specs/array_intersection_spec.rb
@@ -1,5 +1,8 @@
+require 'minitest/skip_dsl'
 require 'minitest/autorun'
 require 'minitest/reporters'
+require 'pry'
+
 require_relative '../lib/array_intersection'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
