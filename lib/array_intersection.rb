dbd1f4e945ca5b9d179d3f29f829d5ca02e56f24
Karinna Iniguez <karinnainiguez@gmail.com>
2018-04-07 12:45:34 -0700

    array_intersection tests passing

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,17 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  inter_array = []
+  return inter_array if array1 == nil || array2 == nil
+
+  array1.length.times do |i|
+
+    array2.length.times do |j|
+      if array1[i] == array2[j]
+        inter_array << array1[i]
+      end
+    end
+
+  end
+
+  return inter_array
 end
