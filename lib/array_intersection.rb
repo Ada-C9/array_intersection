989132d158b460f76350af50bb937a3b0e9e66a6
Maja Graonic <m.graonic@gmail.com>
2018-04-08 15:43:27 -0700

    added complexity notes

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,8 +1,10 @@
 # Creates a new array to return the intersection of the two input arrays
+# Time complexity is O(nm) because the inner loop executes m times for every nth execution of the outer loop
+# Space complexity is O(1) because no additional memory is required for larger input (ie, longer array)
 def intersection(array1, array2)
   array3 = []
   i = 0
-  
+
   return array3 if array1 == nil || array2 == nil
 
   until array1[i] == nil
