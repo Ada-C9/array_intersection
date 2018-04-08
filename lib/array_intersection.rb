d3c7821fce965dde881a71f097adf513a1870cb9
Cheeron Mars <cheeronmars+gh@posteo.org>
2018-04-07 21:05:58 -0700

    remove unnecessary lines of code

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -2,15 +2,12 @@
 def intersection(array1, array2)
   array_intersections = []
   counter_1 = 0
-  counter_2 = 0
   if array1 != nil && array2 != nil
     while array1[counter_1] != nil
       element_1 = array1[counter_1]
-      element_1
       counter_2 = 0
       while array2[counter_2] != nil
         element_2 = array2[counter_2]
-        element_2
         if element_1 == element_2
           array_intersections << element_1
         end
