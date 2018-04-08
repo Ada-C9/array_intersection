e228678aca24a2b17187063b8efe8346f3f3ac0e
Lily Sky <lilyxsky@gmail.com>
2018-04-08 16:42:09 -0700

    refactored code

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -4,19 +4,13 @@ require 'pry'
 def intersection(array1, array2)
   return [] if array1 == nil || array2 == nil
 
-  #find highest possible intersections
-  array1.length < array2.length ? highest_similar = array1.length : highest_similar = array2.length
-
-  #make "restricted" array of nils; find array intersections and put them in the restricted array (replacing the nils)
   index_a = 0
-  similar_possible = Array.new(highest_similar)
   similar_count = 0
 
   array1.length.times do
     index_b = 0
     array2.length.times do
       if array1[index_a] == array2[index_b]
-        similar_possible[index_a] = array1[index_a]
         similar_count += 1
       end
       index_b += 1
@@ -24,18 +18,17 @@ def intersection(array1, array2)
     index_a += 1
   end
 
-  #use helper to reduce array with nils to final array without any nils
-  return results(similar_count, similar_possible)
-end
-
-def results(similar_count, similar_possible)
   index_c = 0
-  index_d = 0
   similar_array = Array.new(similar_count)
+  similar_index = 0
 
-  similar_possible.length.times do
-    if similar_possible[index_c] != nil
-      similar_array[index_d] = similar_possible[index_c]
+  array1.length.times do
+    index_d = 0
+    array2.length.times do
+      if array1[index_c] == array2[index_d]
+        similar_array[similar_index] = array1[index_c]
+        similar_index += 1
+      end
       index_d += 1
     end
     index_c += 1
