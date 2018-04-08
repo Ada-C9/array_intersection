54b930e32cbe8fdda39487d7f2af675a9ffe9a62
Aruna <aruna.thungathurthi@gmail.com>
2018-04-08 14:57:22 -0700

    completed the assignment

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,15 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  intersect_array = []
+  if array1 == nil || array2 == nil
+    return intersect_array
+  end
+  array1.length.times do |i|
+    array2.length.times do |j|
+      if array1[i] == array2[j]
+        intersect_array << array1[i]
+      end
+    end
+  end
+  return intersect_array
 end
