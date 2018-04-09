bacffa531df3358e46dc8b7293041ca303c4b105
Kate Pond <kate@thepondsedge.com>
2018-04-08 20:00:52 -0700

    solves find array intersection

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,17 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  return [] if array1 == nil || array2 == nil
+
+  new_array = []
+
+  array1.each do |num|
+    array2.each do |num2|
+      if num == num2
+        new_array << num2
+      end
+    end
+  end
+
+  return new_array
+
 end
