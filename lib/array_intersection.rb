dc26ea5f0708c3a357b32ee8ba973bf39ac42759
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-09 18:05:52 -0700

    added method to find the union between two sets of integers

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,29 @@
 # Creates a new array to return the intersection of the two input arrays
+require 'pry'
 def intersection(array1, array2)
-  raise NotImplementedError
+  if array1 == nil
+    array1 = []
+  end
+  if array2 == nil
+    array2 = []
+  end
+  counter = 0
+  combined_arr = array1 + array2
+  combining_hash = {}
+  union = []
+
+  while counter < combined_arr.length
+    if combining_hash[ combined_arr[counter] ] == nil
+      combining_hash[combined_arr[counter]] = 1
+    else
+      combining_hash[combined_arr[counter]]+=1
+      if combining_hash[combined_arr[counter]] > 1
+        union << combined_arr[counter]
+      end
+    end
+    counter+=1
+  end
+
+
+  return union
 end
