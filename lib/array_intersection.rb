4fe7efd7c61731fb29ac9cca5e45bf3125633b4f
Kaitlin Forsman <kcforsman@outlook.com>
2018-04-11 11:53:48 -0700

    finished implementation of array intersection with space complexity, O(n), and time complexity of O(n^2)

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,13 @@
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
-  raise NotImplementedError
+  return [] if array1 == nil || array2 == nil || array1.empty? || array2.empty?
+  intersection_array = []
+  array1.length.times do | j |
+    array2.length.times do | k |
+      if array1[j] == array2[k]
+        intersection_array << array1[j]
+      end
+    end
+  end
+  intersection_array
 end
